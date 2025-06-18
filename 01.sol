f074a09ca147cc7f7e4448f8ef104eaa1eadebab
meidawei <2983277940@qq.com>
2025-06-19 02:01:59 +0800

    第一个sol文件提交

--- 01.sol
+++ 01.sol
@@ -1,15 +1,29 @@
-// 从这里开始声明一个合约
-pragma solidity ^0.8.0;
 // SPDX-License-Identifier: MIT
+// 上面得注释如果不加会有报警提示
+pragma solidity ^0.8.0;
 
-// 声明一个合约
+// 可以定义多个合约
+contract Name {
+
+}
+
+// 第一个合约
 contract My {
-    // 合约逻辑
-
+    // 这里是声明一个合约的地方
     // 第一个变量
-    string name = "Hello, World!";
+    string name = "Hello world";
+    event log2(uint256 a1);
 
-    function getName() public view returns (string memory) {
+    // returns
+    function getName() public returns (string memory) {
+        uint256 a = 1;
+        // emit是打印的意思
+        // 打印出来
+        emit log2(a);
+        a = a + 10;
+        emit log2(a);
+        a = a + 20;
+        emit log2(a);
         return name;
     }
 }
